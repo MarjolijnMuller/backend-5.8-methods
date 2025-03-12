8dbc9b852721d7c4f204ba6f4d5924e91fe6f5c0
MarjolijnMuller <polle.marjolijn@gmail.com>
2025-03-12 09:09:37 +0100

    opdrachten gemaakt en werkt

--- src/main/java/B_Dialoog.java
+++ src/main/java/B_Dialoog.java
@@ -6,17 +6,23 @@ public class B_Dialoog {
         // De methode heeft als parameter de tekst die gesproken moet worden.
         // De volgende zin wordt steeds door de ander gesproken.
         // Vervang System.out.println() steeds door je eigen method.
-        System.out.println("Hey"); //Bert
-        System.out.println("Hey"); //Ernie
-        System.out.println("Welcome to this wonderful conversation");
-        System.out.println("Thnx, I'm looking forward to it");
-        System.out.println("How about this weather?");
-        System.out.println("I really don't mind the cold....");
-        System.out.println("And did you see that random program on that random channel?");
-        System.out.println("Well... I did watch a random program on a random channel...");
-        System.out.println("Thank you for a wonderful conversation!");
-        System.out.println("Cya");
+        System.out.println(bertSays("Hey")); //Bert
+        System.out.println(ernieSays("Hey")); //Ernie
+        System.out.println(bertSays("Welcome to this wonderful conversation"));
+        System.out.println(ernieSays("Thnx, I'm looking forward to it"));
+        System.out.println(bertSays("How about this weather?"));
+        System.out.println(ernieSays("I really don't mind the cold...."));
+        System.out.println(bertSays("And did you see that random program on that random channel?"));
+        System.out.println(ernieSays("Well... I did watch a random program on a random channel..."));
+        System.out.println(bertSays("Thank you for a wonderful conversation!"));
+        System.out.println(ernieSays("Cya"));
     }
 
-    // nieuwe methods komen hier
+    public static String bertSays(String sentence){
+        return("Bert says: " + sentence);
+    }
+
+    public static String ernieSays(String sentence) {
+        return(sentence + " says Ernie");
+    }
 }
